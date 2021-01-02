4d81bdb0db485514567c10d2351775ed3052f419
Plarpoon <plarpoon.dev@gmail.com>
2021-01-02 14:48:27 +0100

    gitignore cleanup and update

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+
+namespace WoM_Balance_Bot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+             var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+    }
+}
