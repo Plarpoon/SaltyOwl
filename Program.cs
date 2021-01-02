e9fcd5c036e1926fdc1bb795e4eaf08a7a6d3327
Plarpoon <plarpoon.dev@gmail.com>
2021-01-02 14:01:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WoM_Balance_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
