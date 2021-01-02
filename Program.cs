ecf1e2e836293604d99f24e74ae57f2ea38b4eb2
Plarpoon <plarpoon.dev@gmail.com>
2021-01-02 14:38:23 +0100

    connection to Discord sockets

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Discord;
 
 namespace WoM_Balance_Bot
 {
-    class Program
-    {
-        static void Main(string[] args)
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+             var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
     }
 }
