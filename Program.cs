cf25575ad300394e23a8760e960854aa2fd4499b
Plarpoon <plarpoon.dev@gmail.com>
2021-01-04 05:17:41 +0100

    added Google APIs quickstart

--- Program.cs
+++ Program.cs
@@ -1,25 +1,91 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WoM_Balance_Bot
 {
     public class Program
     {
-        public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        private static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
 
+        private static string ApplicationName = "World Of Mounts balance checker";
+
+        // Instantiates connections.
+        public static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult(); //Discord socket.
+
+            UserCredential credential;  // Google API credentials.
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                                Scopes,
+                            "user",
+                            CancellationToken.None,
+                            new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            Console.Read();
+        }
+
+        // Create Discord API service.
         private DiscordSocketClient _client;
 
+        // Main bot cycle.
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
             _client.MessageReceived += CommandHandler;
             _client.Log += Log;
 
-            var token = File.ReadAllText("token");
+            var token = File.ReadAllText("bot-token.txt");
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
@@ -60,7 +126,7 @@ namespace WoM_Balance_Bot
             {
                 message.Channel.SendMessageAsync($@"Hello {message.Author.Mention}, I am here to assist the WoM staff managing their internal balance!");
             }
-            else if (command.Equals("age"))
+            else if (command.Equals("balance"))
             {
                 message.Channel.SendMessageAsync($@"Your account was created at {message.Author.CreatedAt.DateTime.Date}");
             }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # WoM Check Balance Bot
 ## Discord bot that checks the internal balance of the "World of Mounts" staff
 
-Remember to add a file next to the executable called "token" which must contain the actual bot token from your https://discord.com/developers/ account.
+Remember to add a file next to the executable called "bot-token.txt" (with no extension) which must contain the actual bot token from your https://discord.com/developers/ account.
 
 - use this guide to create one if you haven't yet.
 https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
